897bd3546ca7c6ab9a8f45cc4c9893fcc337dcaf
perezm.josea <perezm.josea@gmail.com>
2018-07-12 20:21:35 +0200

    Details View Init

--- routes/index.js
+++ routes/index.js
@@ -13,8 +13,19 @@ router.get('/feed', async function(req, res, next) {
       apiKey: process.env.NEWS_API_KEY
     }
   }).catch(e => res.status(500).send("error"));
+  res.render('feed', { title: 'NewsReader | Feed', noticias: news.data.articles});
+});
 
-  res.render('feed', { title: 'NewsReader | Feed', noticias: news.data.articles });
+router.get('/details/:index', async function(req, res, next) {
+  let index = req.params.index;
+  const news = await axios.get("https://newsapi.org/v2/top-headlines", {
+    params: {
+      country: "us",
+      apiKey: process.env.NEWS_API_KEY
+    }
+  }).catch(e => res.status(500).send("error"));
+  const noticia = news.data.articles[index];
+  res.render('details', { title: 'NewsReader | Details', noticia:noticia});
 });
 
 module.exports = router;
