9ef30c454411d2d9a35641b7ee90bbea9897fd6b
GonzaloCedillo <goncedillo@gmail.com>
2018-07-14 14:56:21 +0200

    Rating y favoritos

--- public/js/detail.js
+++ public/js/detail.js
@@ -1,24 +1,62 @@
-(function(){
+// OLD SCHOOL
 
-    var $a_inputs_rating = $("input[name=rating]");
-    var idNoticia = $("article").first().data("idnoticia");
+(function() {
+  var $a_inputs_rating = $("input[name=rating]");
+  var idNoticia = $("article")
+    .first()
+    .data("idnoticia");
 
-    $a_inputs_rating.change(function(){
-        console.log("Update rating");
-        updateRating(idNoticia, parseFloat(this.value));
+  // Seteamos rating
+  var rate = $("article").attr("data-rate");
+  var $inputRate = $(`input[value=${rate}]`);
+  $inputRate.attr("checked", "checked");
+
+  // Trabajamos el Fav
+  var $fav = $(".fav");
+  $fav.on("click", e => {
+    $.ajax({
+      url: `/update-fav/${idNoticia}`,
+      type: "patch",
+      success: resp => $fav.toggleClass("has-text-danger")
     });
+  });
 
-    function updateRating(idNoticia, rating){
+  // Eventos de Rating
+  $a_inputs_rating.change(function() {
+    updateRating(idNoticia, parseFloat(this.value));
+  });
 
-        $.ajax({
-            data: {
-                id: idNoticia,
-                rating: rating
-            },
-            url: "/update-rating",
-            type: "post"
-        });
+  function updateRating(idNoticia, rating) {
+    $.ajax({
+      data: {
+        rating: rating
+      },
+      url: `/update-rating/${idNoticia}`,
+      type: "patch"
+    });
+  }
+})();
 
-    }
-    
-})()
+// const inputsRating = document.querySelectorAll("input[name=rating]");
+// const idNoticia = document
+//   .querySelector("article")
+//   .getAttribute("data-idnoticia");
+
+// function updateRating(e) {
+//   fetch("/update-rating", {
+//     method: "patch",
+//     mode: "cors",
+//     body: JSON.stringify({
+//       id: idNoticia,
+//       rating: e.currentTarget.value
+//     }),
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json"
+//     }
+//   })
+//     .then(resp => console.log("OK"))
+//     .catch(e => console.log("ERROR", e));
+// }
+
+// inputsRating.forEach(item => item.addEventListener("change", updateRating));
