1b4d97edb8970eefd0dbc785d48b768664dceaab
GonzaloCedillo <goncedillo@gmail.com>
2018-07-14 12:37:51 +0200

    Pintando detalle

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,64 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
+const uuid = require("uuid/v3");
+const dbPath = path.join(__dirname, "../db.json");
+// !AVISO Necesitamos un /db.json ==> { "articles": [] }
+const db = JSON.parse(fs.readFileSync(dbPath), "utf8");
 
-router.get('/', function(req, res, next) {
-  res.render('landing', { title: 'NewsReader' });
+function saveOnDB(data) {
+  const oldNews = db.articles;
+  const totalNews = [...oldNews, ...data]; // db.articles.concat(data)
+  const dataToWrite = { articles: totalNews };
+  const dataString = JSON.stringify(dataToWrite);
+
+  fs.writeFileSync(dbPath, dataString, "utf8");
+}
+
+router.get("/", function(req, res, next) {
+  res.render("landing", { title: "NewsReader" });
 });
 
-router.get('/feed', async function(req, res, next) {
-  const news = await axios.get("https://newsapi.org/v2/top-headlines", {
-    params: {
-      country: "us",
-      apiKey: process.env.NEWS_API_KEY
-    }
-  }).catch(e => res.status(500).send("error"));
+router.get("/feed", async function(req, res) {
+  const news = await axios
+    .get("https://newsapi.org/v2/top-headlines", {
+      params: {
+        country: "us",
+        apiKey: process.env.NEWS_API_KEY
+      }
+    })
+    .catch(e => res.status(500).send("error"));
 
-  res.render('feed', { title: 'NewsReader | Feed', noticias: news.data.articles });
+  const totalArticles = news.data.articles.map(article => ({
+    ...article,
+    id: uuid(article.url, uuid.URL),
+    rating: 0
+  }));
+
+  const articlesFiltered = totalArticles.filter(item => {
+    const check = db.articles.find(art => art.url === item.url);
+    // Quiero devolver el contrario de la comprobación
+    // Si encuentro el artículo por URL, entonces es un false (no quiero duplicar)
+    return !Boolean(check);
+  });
+
+  // Pinto en pantalla todos los que me vienen
+  res.render("feed", {
+    title: "NewsReader | Feed",
+    noticias: totalArticles
+  });
+
+  // Guardo solo los que no tenía guardados antes
+  saveOnDB(articlesFiltered);
+});
+
+router.get("/detail/:id", async function(req, res) {
+  const param = req.params.id;
+  const article = db.articles.find(item => item.id === param);
+
+  res.render("detail", { title: "NewsReader | Detail", article });
 });
 
 module.exports = router;
