8c318fb8d725fcaaa28447ec7e1cd3da6488da8d
JaviMuru <javimuruzabal@hotmail.com>
2018-07-18 17:33:26 +0200

    Implement nav routes and filtered by category

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,34 @@ const dbPath = path.join(__dirname, "../db.json");
 // !AVISO Necesitamos un /db.json ==> { "articles": [] }
 const db = JSON.parse(fs.readFileSync(dbPath), "utf8");
 
-function saveOnDB(data) {
-  const oldNews = db.articles;
-  const totalNews = [...oldNews, ...data]; // db.articles.concat(data)
-  const dataToWrite = { articles: totalNews };
-  const dataString = JSON.stringify(dataToWrite);
+function saveOnDB(data,section) {
+  let oldNews;
+  let totalNews;
+  let dataToWrite;
+  switch (section) {
+    case 'general':
+    oldNews = db.general;
+    db.general = [...oldNews, ...data];
+    dataToWrite = db;
+    break;
+    case 'sports':
+    oldNews = db.sports;
+    db.sports = [...oldNews, ...data];
+    dataToWrite = db;
+    break;
+    case 'entertainment':
+    oldNews = db.entertainment;
+    db.entertainment = [...oldNews, ...data];
+    dataToWrite = db;
+    break;
+    case 'business':
+    oldNews = db.business;
+    db.business = [...oldNews, ...data];
+    dataToWrite = db;
+    break;
+  }
 
+  const dataString = JSON.stringify(db);
   fs.writeFileSync(dbPath, dataString, "utf8");
 }
 
@@ -39,7 +61,7 @@ router.get("/feed", async function(req, res) {
   }));
 
   const articlesFiltered = totalArticles.filter(item => {
-    const check = db.articles.find(art => art.url === item.url);
+    const check = db.general.find(art => art.url === item.url);
     // Quiero devolver el contrario de la comprobación
     // Si encuentro el artículo por URL, entonces es un false (no quiero duplicar)
     return !Boolean(check);
@@ -48,6 +70,142 @@ router.get("/feed", async function(req, res) {
   // Pinto en pantalla todos los que me vienen
   res.render("feed", {
     title: "NewsReader | Feed",
+    noticias: totalArticles
+  });
+
+  // Guardo solo los que no tenía guardados antes
+  saveOnDB(articlesFiltered, 'general');
+});
+
+router.get("/feed/sports", async function(req, res) {
+  const news = await axios
+    .get("https://newsapi.org/v2/top-headlines?category=sports", {
+      params: {
+        country: "us",
+        apiKey: process.env.NEWS_API_KEY
+      }
+    })
+    .catch(e => res.status(500).send("error"));
+
+  const totalArticles = news.data.articles.map(article => ({
+    ...article,
+    id: uuid(article.url, uuid.URL),
+    rating: 0,
+    fav: false
+  }));
+
+  const articlesFiltered = totalArticles.filter(item => {
+    const check = db.sports.find(art => art.url === item.url);
+    // Quiero devolver el contrario de la comprobación
+    // Si encuentro el artículo por URL, entonces es un false (no quiero duplicar)
+    return !Boolean(check);
+  });
+
+  // Pinto en pantalla todos los que me vienen
+  res.render("feed", {
+    title: "NewsReader | Sports",
+    noticias: totalArticles
+  });
+
+  // Guardo solo los que no tenía guardados antes
+  saveOnDB(articlesFiltered, 'sports');
+});
+
+router.get("/feed/general", async function(req, res) {
+  const news = await axios
+    .get("https://newsapi.org/v2/top-headlines?category=general", {
+      params: {
+        country: "us",
+        apiKey: process.env.NEWS_API_KEY
+      }
+    })
+    .catch(e => res.status(500).send("error"));
+
+  const totalArticles = news.data.articles.map(article => ({
+    ...article,
+    id: uuid(article.url, uuid.URL),
+    rating: 0,
+    fav: false
+  }));
+
+  const articlesFiltered = totalArticles.filter(item => {
+    const check = db.general.find(art => art.url === item.url);
+    // Quiero devolver el contrario de la comprobación
+    // Si encuentro el artículo por URL, entonces es un false (no quiero duplicar)
+    return !Boolean(check);
+  });
+
+  // Pinto en pantalla todos los que me vienen
+  res.render("feed", {
+    title: "NewsReader | General",
+    noticias: totalArticles
+  });
+
+  // Guardo solo los que no tenía guardados antes
+  saveOnDB(articlesFiltered);
+});
+
+router.get("/feed/business", async function(req, res) {
+  const news = await axios
+    .get("https://newsapi.org/v2/top-headlines?category=business", {
+      params: {
+        country: "us",
+        apiKey: process.env.NEWS_API_KEY
+      }
+    })
+    .catch(e => res.status(500).send("error"));
+
+  const totalArticles = news.data.articles.map(article => ({
+    ...article,
+    id: uuid(article.url, uuid.URL),
+    rating: 0,
+    fav: false
+  }));
+
+  const articlesFiltered = totalArticles.filter(item => {
+    const check = db.business.find(art => art.url === item.url);
+    // Quiero devolver el contrario de la comprobación
+    // Si encuentro el artículo por URL, entonces es un false (no quiero duplicar)
+    return !Boolean(check);
+  });
+
+  // Pinto en pantalla todos los que me vienen
+  res.render("feed", {
+    title: "NewsReader | Business",
+    noticias: totalArticles
+  });
+
+  // Guardo solo los que no tenía guardados antes
+  saveOnDB(articlesFiltered);
+});
+
+router.get("/feed/entertainment", async function(req, res) {
+  const news = await axios
+    .get("https://newsapi.org/v2/top-headlines?category=entertainment", {
+      params: {
+        country: "us",
+        apiKey: process.env.NEWS_API_KEY
+      }
+    })
+    .catch(e => res.status(500).send("error"));
+
+  const totalArticles = news.data.articles.map(article => ({
+    ...article,
+    id: uuid(article.url, uuid.URL),
+    rating: 0,
+    fav: false
+  }));
+
+  const articlesFiltered = totalArticles.filter(item => {
+    const check = db.entertainment.find(art => art.url === item.url);
+    // Quiero devolver el contrario de la comprobación
+    // Si encuentro el artículo por URL, entonces es un false (no quiero duplicar)
+    return !Boolean(check);
+  });
+
+  // Pinto en pantalla todos los que me vienen
+  res.render("feed", {
+    title: "NewsReader | Entertainment",
     noticias: totalArticles
   });
 
